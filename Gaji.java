780ec3caceace71b727aa9e2e5499a22e09dc4af
kaisar rozan b <kaisarrozanbasalika891@gmail.com>
2024-12-10 11:43:32 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,7 +1,25 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Gaji{
     public static int masaKerja;
+
+    public static int gaji(int masaKerja){
+    if (masaKerja<5) {
+        return 5000000;
+    } else if (masaKerja>=5&&masaKerja<=10) {
+        return 7500000;
+    } else 
+    return 10000000;
+    }
+
+    public static int lembur(int masaKerja){
+        if (masaKerja<5) {
+            return 40000;
+        } else if (masaKerja>=5&&masaKerja<=10) {
+            return 50000;
+        } else 
+        return 75000;
+    }
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
@@ -10,11 +28,20 @@ public class Gaji{
 
         /* Pekerjaan anda mulai dari sini */
 
+        int gajiBulanan = gaji(masaKerja);
+        int lemburBulanan = lembur(masaKerja);
+
+        lemburBulanan*=jamLembur;
+        gajiBulanan+=lemburBulanan;
+
+        System.out.println(gajiBulanan);
+        
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Gaji.class.getDeclaredMethods().length);
         System.out.println(Gaji.class.getDeclaredFields().length);
         
+        input.close();
     }
     
 }
